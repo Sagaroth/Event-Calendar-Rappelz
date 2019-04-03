bde2d278bba0eb5f99f5c6727fa6e6e2c3cf2bb1
Sagaroth <sagaaskaard@gmail.com>
2019-04-03 21:36:54 +0200

    Redesign sign query

---
+++ connect/db_connect.php
@@ -0,0 +1,29 @@
+<?php
+/*  Rappelz Event Calendar  - Make events with players.>
+    Copyright (C) <2019>  <History of Rappelz>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+/* Database connection start */
+$servername = "localhost";
+$username = "dba_calendar_event";
+$password = "55npMvH9oJhuRvW4";
+$dbname = "calendar";
+$conn = mysqli_connect($servername, $username, $password, $dbname) or die("Connection failed: " . mysqli_connect_error());
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+
+?>

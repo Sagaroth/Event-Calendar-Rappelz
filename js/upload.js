23c216f210add493dc8d2c5c5d799d89932efb47
Sagaroth <sagaaskaard@gmail.com>
2019-04-03 21:17:05 +0200

    Initial commit

--- js/upload.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-	$(".dropzone").dropzone({
-	  url: 'event/addEvent.php',
-	  /*width: '100%',*/
-	  /*height: 300,*/ 
-	  progressBarWidth: '100%',
-	  maxFileSize: '5MB',
-	})
-});
--- welcome.php
+++
@@ -1,4 +0,0 @@
-<?php
-	echo "<div class='alert alert-success'><center><p style='margin-top: 12px;'>	Vous êtes désormait inscrit sur la liste des organisateurs d'Events !</p></center></div>";	
-?>	
-

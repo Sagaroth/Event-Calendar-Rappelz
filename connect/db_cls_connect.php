df88216ef5b540426bacfeaac2edf360c74c5174
Sagaroth <sagaaskaard@gmail.com>
2019-04-03 21:40:50 +0200

    Revert "Redesign sign query"
    This reverts commit bde2d278bba0eb5f99f5c6727fa6e6e2c3cf2bb1.

--- connect/db_cls_connect.php
+++
@@ -1,4 +0,0 @@
-<?php
-session_start(); //Start SESSION for response.php and sign.php usage.
-$link = new PDO('mysql:host=localhost;dbname=calendar;charset=utf8','dba_calendar_event','55npMvH9oJhuRvW4');
-?>

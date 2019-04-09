00a388218e89a282756a26a1f8e84d9bf2d74a74
Sagaroth <gpotin@expert-line.com>
2019-04-09 16:40:25 +0200

    Modification message erreur inscription

--- js/sign.js
+++ js/sign.js
@@ -64,7 +64,7 @@ $('document').ready(function() {
 
         } else {          
          	$("#error").fadeIn(1000, function(){           
-      			$("#error").html('<div class="alert alert-danger"><span class="glyphicon glyphicon-info-sign"></span> &nbsp; '+data+' !</div>');           
+      			$("#error").html('<div class="alert alert-danger"><span class="glyphicon glyphicon-info-sign"></span> &nbsp; Il y a eu un problème lors de votre inscription, nous vous invitons à réessayer !</div>');           
          		$("#btn-sign").html('<span class="glyphicon glyphicon-log-in"></span> &nbsp; M\'inscrire');         
          	});           
        	}
